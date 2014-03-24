d4e0ade105184f0063963724cdefcfb8d030df89
Reyuu <rey444xd3@gmail.com>
2014-03-24 16:05:30 +0100

    rewritten readbuffer

--- socket_x1.py
+++ socket_x1.py
@@ -1,20 +1,27 @@
-import socket, sys, random, string
+import sys, socket, random, string, time
 from configirc import *
 
 readbuffer = ""
+stateTrueLoop = True
 
 irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 irc.connect((HOST, PORT))
 irc.send("NICK %s\r\n" % NICK)
 irc.send("USER %s %s bla :%s\r\n" % (IDENT, HOST, REALNAME))
-irc.send("JOIN %s\r\n" % (JOIN)
+time.sleep(5)
+irc.send("JOIN %s\r\n" % (JOIN))
 
-while True:
+while stateTrueLoop == True:
+    irc.settimeout(TIMEOUTTIME)
     readbuffer = readbuffer + irc.recv(1024)
-    readbuffer = temp.pop( )
+    if 'PING' in readbuffer:
+        print strftime("[*] [%H:%M:%S] Pinged and ponged", gmtime())
+    temp = string.split(readbuffer, "\n")
+
     for line in temp:
         line = string.rstrip(line)
         line = string.split(line)
         if(line[0] == "PING"):
             irc.send("PONG %s\r\n" % line[1])
+
 
