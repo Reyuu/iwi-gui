180eadd507db2e5025c7f5f7a94336155692c9f0
Reyuu <rey444xd3@gmail.com>
2014-03-25 18:05:56 +0100

    rewritten everything in class - easier to use

--- socket_x1.py
+++ socket_x1.py
@@ -1,33 +1,38 @@
 import sys, socket, random, string, time
+global HOST, PORT, NICK, IDENT, REALNAME, CHAN, TIMEOUTTIME
 execfile("configirc.ini")
-
-readbuffer = ""
-stateTrueLoop = True
-onChannelMsg = 'PRIVMSG %s :' % (CHAN)
-
-irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-irc.connect((HOST, PORT))
-irc.send("NICK %s\r\n" % NICK)
-irc.send("USER %s %s bla :%s\r\n" % (IDENT, HOST, REALNAME))
-time.sleep(5)
-irc.send("JOIN %s\r\n" % (CHAN))
-irc.settimeout(TIMEOUTTIME)
-
-while stateTrueLoop == True:
-    try:
-        readbuffer = irc.recv(1024)
-    except:
+class Irc:
+    def __init__(self):
         readbuffer = ""
-    temp = string.split(readbuffer, "\n")
-    for line in temp:
+        onChannelMsg = 'PRIVMSG %s :' % (CHAN)
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    def connect(self):
+        self.socket.connect((HOST, PORT))
+        self.socket.send("NICK %s\r\n" % NICK)
+        self.socket.send("USER %s %s bla :%s\r\n" % (IDENT, HOST, REALNAME))
+        time.sleep(5)
+        self.socket.send("JOIN %s\r\n" % (CHAN))
+        self.socket.settimeout(TIMEOUTTIME)
+    def whileSection(self):
         try:
-            line = string.rstrip(line)
-            line = string.split(line)
-
-            if(line[0] == "PING"):
-                irc.send("PONG %s\r\n" % line[1])
-                print "Pinged and ponged"
-            print ' '.join(line)  # putting this at end of try;except clause because it
-            #  crashes on empty lines
+            readbuffer = self.socket.recv(1024)
         except:
-            pass
+            readbuffer = ""
+        temp = string.split(readbuffer, "\n")
+        for line in temp:
+            try:
+                line = string.rstrip(line)
+                line = string.split(line)
+                if(line[0] == "PING"):
+                    self.socket.send("PONG %s\r\n" % line[1])
+                    print "Pinged and ponged"
+                print ' '.join(line)  # putting this at end of try;except clause because it crashes on empty lines
+            except:
+                pass
+
+IrcC = Irc()
+IrcC.connect()
+while True:
+    IrcC.whileSection()
+
+
