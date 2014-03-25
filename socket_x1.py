5392173aa3ceedcfda848ffa3bf11a651bea0d46
Reyuu <rey444xd3@gmail.com>
2014-03-25 12:32:43 +0100

    minor changes

--- socket_x1.py
+++ socket_x1.py
@@ -1,22 +1,22 @@
 import sys, socket, random, string, time
-import classesx1
 execfile("configirc.ini")
 
 readbuffer = ""
 stateTrueLoop = True
+onChannelMsg = 'PRIVMSG %s :' % (CHAN)
 
 irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 irc.connect((HOST, PORT))
 irc.send("NICK %s\r\n" % NICK)
 irc.send("USER %s %s bla :%s\r\n" % (IDENT, HOST, REALNAME))
 time.sleep(5)
-irc.send("JOIN %s\r\n" % (JOIN))
+irc.send("JOIN %s\r\n" % (CHAN))
 
 while stateTrueLoop == True:
     try:
         readbuffer = readbuffer + irc.recv(1024)
     except readbuffer == "":
-        print('Where\'s data!')
+        print('Where\'s data! Not connected or wrong server adress')
         break
     except:
         print('I donno what happen')
@@ -24,13 +24,15 @@ while stateTrueLoop == True:
 
         
     if 'PING' in readbuffer:
-        print("[*] [%H:%M:%S] Pinged and ponged", time.gmtime())
+        print("Pinged and ponged")
     temp = string.split(readbuffer, "\n")
     print(temp)
+
     for line in temp:
         try:
             line = string.rstrip(line)
             line = string.split(line)
+
             if(line[0] == "PING"):
                 irc.send("PONG %s\r\n" % line[1])
         except:
