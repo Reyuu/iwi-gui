ae96ca5a6813fb252e01ac6eb13276c25ea8329a
maciej01 <maciej@maciej01.tk>
2014-03-29 21:36:37 +0100

    Adding commands

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
-Attempt to create simple irc framework.
-This is just a proof of concept.
-Working on sockets.
+Attempt to create simple irc framework.    
+This is just a proof of concept.    
+Working on sockets.    
 
-Many thanks to maciej01.
+Many thanks to maciej01.    
+
+Commands:    
+:j channel - joins a channel    
+:q - quits the bot    
+:ch channel - sets the default channel to send messages and output to specified channel    
+:l message - sends back the previous message, the parameter is not necessary, it appends it at end    
+:p user/channel message - sends a private message to specified user or channel    
+:r input - sends raw messages to server, for ex PRIVMSG #channel :message    
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import sys
+import os
 import threading
+import socket_x1
 from socket_x1 import *
-global IrcC
-IrcC = Irc()
+global IrcC, CHAN
+IrcC = socket_x1.Irc()
 
 class IrcThread (threading.Thread):
     def __init__(self):
@@ -15,24 +17,51 @@ class IrcThread (threading.Thread):
 
 class InputThreadIrc (threading.Thread):
     def __init__(self):
+        global CHAN
         threading.Thread.__init__(self)
-
+        socket_x1.fetchSettings()
+        self.lastMessage = [CHAN, '']
+        
     def run(self):
+        global CHAN
         while 1:
             try:
                 self.line2 = sys.stdin.readline()
             except KeyboardInterrupt:
                 break
+            execute, specialChan = True, CHAN
+            if self.line2[0] == ':':
+                inputArray = self.line2.replace('\n', '').split(' ')
+                command = inputArray[0]
+                command = command[1:]
+                if command == 'q': # quit
+                    os._exit(1)
+                    break
+                elif command == 'l': # last message, eventual append
+                    self.line2 = self.lastMessage[1]
+                    specialChan = self.lastMessage[0]
+                    if len(inputArray) > 1:
+                        self.line2 = self.line2 + ' ' + ' '.join(inputArray[1:])
+                        self.line2 = self.line2.replace('\n', '')
+                elif command == 'p': # private message to specified user/chan
+                    specialChan = inputArray[1]
+                    self.line2 = ' '.join(inputArray[2:])
+                elif command == 'r': # raw input to server
+                    execute = False
+                    IrcC.send(' '.join(inputArray[1:]))
+                elif command == 'j': # join to a channel
+                    execute = False
+                    IrcC.send('JOIN '+inputArray[1])
+                elif command == 'ch':
+                    socket_x1.CHAN = inputArray[1]
+                    CHAN = inputArray[1]
+                    execute = False
 
-            if(self.line2 == "QUIT"):
-                sys.exit()
-
-            elif not self.line2:
-                break
-
-            else:
-                IrcC.logger.info(" ["+NICK+"] "+self.line2+" to "+CHAN+":")
-                IrcC.sendMsg(CHAN, self.line2)
+            if execute:
+                IrcC.logger.info(" ["+NICK+"] "+self.line2+" to "+specialChan+":")
+                IrcC.sendMsg(specialChan, self.line2)
+                self.lastMessage[1] = self.line2
+                self.lastMessage[0] = specialChan
 
 thread1 = IrcThread()
 thread2 = InputThreadIrc()
