259b46d3ad105911829e713084f29fb169cc8623
maciej01 <maciej@maciej01.tk>
2014-04-02 22:40:16 +0200

    tkinter > > > > urwid > >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> curses

--- main.py
+++ main.py
@@ -1,10 +1,92 @@
+# -*- coding: utf-8 -*-
 import sys
 import os
 import threading
 import socket_x1
 from socket_x1 import *
-global IrcC, CHAN
+import Tkinter as tk
+import tkFont
+
+global IrcC
 IrcC = socket_x1.Irc()
+
+def cbc(idd, tex):
+    return lambda : callback(idd, tex)
+
+def callback(text, tex):
+    tex.insert(tk.END, str(text)+'\n')
+    tex.see(tk.END)             # Scroll if necessary
+
+def click(key):
+    try:
+        if ord(key.char) == 13: # enter
+            text = E1.get()
+            E1.delete(0, tk.END)
+            execute, specialChan = True, socket_x1.CHAN
+            IrcC.variables['hl'] = IrcC.lastHL
+            IrcC.variables['lm'] = IrcC.lastMessage[1]
+            if text[0] == ':':
+                inputArray = text.replace('\n', '').split(' ')
+                command = inputArray[0]
+                command = command[1:]
+                if command == 'q': # quit
+                    os._exit(1)
+                    return 0
+                elif command == 'l': # last message, eventual append
+                    text = IrcC.lastMessage[1]
+                    specialChan = IrcC.lastMessage[0]
+                    if len(inputArray) > 1:
+                        text = text + ' ' + ' '.join(inputArray[1:])
+                        text = text.replace('\n', '')
+                elif command == 'p': # private message to specified user/chan
+                    specialChan = inputArray[1]
+                    text = ' '.join(inputArray[2:])
+                elif command == 'r': # raw input to server
+                    execute = False
+                    IrcC.send(' '.join(inputArray[1:]))
+                elif command == 'j': # join to a channel
+                    execute = False
+                    IrcC.send('JOIN '+inputArray[1])
+                elif command == 'ch': # changes channel
+                    socket_x1.socket_x1.CHAN = inputArray[1]
+                    socket_x1.CHAN = inputArray[1]
+                    execute = False
+                elif command == 'ms': # sets a message to reuse
+                    IrcC.messages[inputArray[1]] = ' '.join(inputArray[2:])
+                    execute = False
+                elif command == 'md': # displays a message
+                    if inputArray[1] in IrcC.messages:
+                        text = IrcC.messages[inputArray[1]]
+                    else:
+                        execute = False
+                elif command == 'vs': # sets a variable
+                    IrcC.variables[inputArray[1]] = ' '.join(inputArray[2:])
+                    execute = False
+                elif command == 'v': # sends a message with variables
+                    words = inputArray[1:]
+                    new_msg = ''
+                    for word in words:
+                        if word[0] == '$':
+                            wording = word[1:].replace(',', '')
+                            wording = wording.replace(':', '')
+                            if wording in IrcC.variables:
+                                new_msg += IrcC.variables[wording]
+                            if word[-1] == ',':
+                                new_msg += ','
+                            elif word[-1] == ':':
+                                new_msg += ':'
+                        else:
+                            new_msg += word
+                        new_msg += ' '
+                    text = new_msg
+            if execute:
+                IrcC.logger.info(" ["+NICK+"] "+text+" to "+specialChan+":")
+                IrcC.sendMsg(specialChan, text)
+                IrcC.lastMessage[1] = text
+                IrcC.lastMessage[0] = specialChan
+    except TypeError:
+        pass
+
 
 class IrcThread (threading.Thread):
     def __init__(self):
@@ -15,89 +97,24 @@ class IrcThread (threading.Thread):
         IrcC.whileSection()
 
 
-class InputThreadIrc (threading.Thread):
-    def __init__(self):
-        global CHAN
-        threading.Thread.__init__(self)
-        socket_x1.fetchSettings()
-        self.lastMessage = [CHAN, '']
-        self.messages = {}
-        self.variables = {}
+arial = ("Arial", "10")
 
-    def run(self):
-        global CHAN
-        while 1:
-            try:
-                self.line2 = sys.stdin.readline()
-            except KeyboardInterrupt:
-                break
-            execute, specialChan = True, CHAN
-            self.variables['hl'] = IrcC.lastHL
-            self.variables['lm'] = self.lastMessage[1]
-            if self.line2[0] == ':':
-                inputArray = self.line2.replace('\n', '').split(' ')
-                command = inputArray[0]
-                command = command[1:]
-                if command == 'q': # quit
-                    os._exit(1)
-                    break
-                elif command == 'l': # last message, eventual append
-                    self.line2 = self.lastMessage[1]
-                    specialChan = self.lastMessage[0]
-                    if len(inputArray) > 1:
-                        self.line2 = self.line2 + ' ' + ' '.join(inputArray[1:])
-                        self.line2 = self.line2.replace('\n', '')
-                elif command == 'p': # private message to specified user/chan
-                    specialChan = inputArray[1]
-                    self.line2 = ' '.join(inputArray[2:])
-                elif command == 'r': # raw input to server
-                    execute = False
-                    IrcC.send(' '.join(inputArray[1:]))
-                elif command == 'j': # join to a channel
-                    execute = False
-                    IrcC.send('JOIN '+inputArray[1])
-                elif command == 'ch': # changes channel
-                    socket_x1.CHAN = inputArray[1]
-                    CHAN = inputArray[1]
-                    execute = False
-                elif command == 'ms': # sets a message to reuse
-                    self.messages[inputArray[1]] = ' '.join(inputArray[2:])
-                    execute = False
-                elif command == 'md': # displays a message
-                    if inputArray[1] in self.messages:
-                        self.line2 = self.messages[inputArray[1]]
-                    else:
-                        execute = False
-                elif command == 'vs': # sets a variable
-                    self.variables[inputArray[1]] = ' '.join(inputArray[2:])
-                    execute = False
-                elif command == 'v': # sends a message with variables
-                    words = inputArray[1:]
-                    new_msg = ''
-                    for word in words:
-                        if word[0] == '$':
-                            wording = word[1:].replace(',', '')
-                            wording = wording.replace(':', '')
-                            if wording in self.variables:
-                                new_msg += self.variables[wording]
-                            if word[-1] == ',':
-                                new_msg += ','
-                            elif word[-1] == ':':
-                                new_msg += ':'
-                        else:
-                            new_msg += word
-                        new_msg += ' '
-                    self.line2 = new_msg
-            if execute:
-                IrcC.logger.info(" ["+NICK+"] "+self.line2+" to "+specialChan+":")
-                IrcC.sendMsg(specialChan, self.line2)
-                self.lastMessage[1] = self.line2
-                self.lastMessage[0] = specialChan
+top = tk.Tk()
+tex = tk.Text(master=top, width=80, height=23, font=arial, bg='#1c1d19', fg='#009a00')
+tex.pack(side=tk.TOP)
+bop = tk.Frame()
+bop.pack(side=tk.BOTTOM)
+E1 = tk.Entry(bop, bd=3, width=80, font=arial, bg='#1c1d19', fg='#009a00')
+E1.bind("<Key>", click)
+E1.pack()
+
+IrcC.pointer = callback
+IrcC.tex = tex
 
 thread1 = IrcThread()
-thread2 = InputThreadIrc()
-thread2.daemon = 1
 
 thread1.start()
-thread2.start()
+arial = ("Arial", "10")
+
+top.mainloop()
 
--- socket_x1.py
+++ socket_x1.py
@@ -5,6 +5,7 @@ from time import gmtime, strftime
 global HOST, PORT, NICK, IDENT, REALNAME, CHAN, TIMEOUTTIME, PING, PLUGINFILE, MASTERS, counter, TrueMaster, NoticeMsgOnChannelJoin, NoticeMsgOnChannelJoinOn, HighLight
 
 BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE = range(8)
+CHAN = '#polish'
 counter = 0
 def fetchSettings():
     config = ConfigParser.ConfigParser()
@@ -33,20 +34,8 @@ def fetchSettings():
     except:
             print "[!] Error have happened while fetching settings from configirc.ini!"
             sys.exit(1)
-def has_colours(stream):
-    if not hasattr(stream, "isatty"):
-        return False
-    if not stream.isatty():
-        return False # auto color only on TTYs
-    try:
-        import curses
-        curses.setupterm()
-        return curses.tigetnum("colors") > 2
-    except:
-        # guess false in case of error
-        return False
-has_colours = has_colours(sys.stdout)
 
+has_colours = False
 def multi_detect(string, inputArray):
     for item in inputArray:
         if item in string:
@@ -54,23 +43,35 @@ def multi_detect(string, inputArray):
             return 1
     return 0
 
-def print_date(msg, colour=YELLOW):
+def print_date(pointer, msg, colour=YELLOW):
     if has_colours:
         seq = "\x1b[1;%dm" % (30+colour) + strftime("[*] [%H:%M:%S] ",gmtime()) + "\x1b[0m"
-        print seq+msg
+        pointer.pointer(seq+msg, pointer.tex)
     else:
-        print strftime("[*] [%H:%M:%S] "+msg, gmtime()) 
+        pointer.pointer(strftime("[*] [%H:%M:%S] "+msg, gmtime()) , pointer.tex)
 
 class Irc:
     def __init__(self):
-        self.onChannelMsg = 'Sup cunts.'
+        self.onChannelMsg = 'hi'
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.lastHL = 'Null'
+        self.variables, self.messages = {}, {}
+        self.lastMessage = ['rey44', 'elo']
     def send(self, msg):
         self.socket.send(msg + "\r\n")
     def sendMsg(self, chan, msg):
-        self.socket.send('PRIVMSG '+chan+' :'+msg+'\r\n')
-        print_date('[%s] to <%s>: %s' % (NICK, chan, msg), colour=BLUE)
+        try:
+            self.socket.send('PRIVMSG '+chan+' :'+unicode(msg)+'\r\n')
+            print_date(self, '[%s] to <%s>: %s' % (NICK, chan, msg), colour=BLUE)
+        except UnicodeEncodeError:
+            a = u""
+            for char in msg:
+                try:
+                    a += char.decode('ascii')
+                except UnicodeEncodeError:
+                    a += "?"
+            self.socket.send('PRIVMSG '+chan+' :'+unicode(a)+'\r\n')
+            print_date(self, '[%s] to <%s>: %s' % (NICK, chan, a), colour=BLUE)
 
     def connect(self):
         #config_fetch()# just couldn't get it to work
@@ -107,7 +108,7 @@ class Irc:
                     if line[0] == "PING":
                         self.send("PONG %s" % line[1])
                         if PING:
-                            print_date("Pinged and ponged.", colour=CYAN)
+                            print_date(self, "Pinged and ponged.", colour=CYAN)
                         else:
                             pass
                     if line[1] == "PRIVMSG":
@@ -118,20 +119,20 @@ class Irc:
                         if hld:
                             self.lastHL = username
                         colour = {0:YELLOW, 1:RED}[(hld == True) or (channel == TrueMaster)]
-                        print_date("[%s] to <%s>: %s" % (username, channel, message), colour=colour)
+                        print_date(self, "[%s] to <%s>: %s" % (username, channel, message), colour=colour)
                         execfile(PLUGINFILE)
                     elif line[1] == "JOIN":
                         username = (line[0].split('!')[0])[1:]
                         if NoticeMsgOnChannelJoinOn == 1:
                             self.send("NOTICE "+username+" :"+NoticeMsgOnChannelJoin)
-                        print_date("[%s] joined the channel <%s>" % (username, ' '.join(line[2:])[1:]), colour=MAGENTA)
+                        print_date(self, "[%s] joined the channel <%s>" % (username, ' '.join(line[2:])[1:]), colour=MAGENTA)
                     elif line[1] == "QUIT":
                         username = (line[0].split('!')[0])[1:]
-                        print_date("[%s] has quit: %s" % (username, ' '.join(line[2:])[1:]), colour=MAGENTA)
+                        print_date(self, "[%s] has quit: %s" % (username, ' '.join(line[2:])[1:]), colour=MAGENTA)
                     elif line[1] == "PART":
                         username = (line[0].split('!')[0])[1:]
                         channel = line[2]
-                        print_date("[%s] leaves from <%s>" % (username, channel), colour=MAGENTA)
+                        print_date(self, "[%s] leaves from <%s>" % (username, channel), colour=MAGENTA)
                     else:
                         print ' '.join(line)
                 except IndexError:
